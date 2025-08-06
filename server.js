22946125894197aae4ba763e7606c58dc0feec18
NavithaGoud <peddinavithagoud@gmail.com>
2025-08-06 20:45:41 +0530

    added in  server.js file

--- server.js
+++ server.js
@@ -38,9 +38,10 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-app.get("*",(req,res)=>{
-  res.sendFile("./client/build/index.html")
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
